f32f613db19a4af1d601729c938294243113bdc9
user <olesyaholod@mail.ru>
2026-03-28 09:15:42 +0300

    Revert "first commit"
    This reverts commit 04c5e7ebfd305a174d93866092a98f8d127f2174.

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,11 +1,7 @@
 package daysteps
 
 import (
-	"fmt"
 	"time"
-    "strings"
-	"strconv"
-	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -15,60 +11,10 @@ const (
 	mInKm = 1000
 )
 
-// parsePackage разбирает строку формата "шаги,длительность"
 func parsePackage(data string) (int, time.Duration, error) {
-	
-	parts := strings.Split(data, ",")
-	
-	// Проверяем что получили ровно 2 части
-	if len(parts) != 2 {
-		return 0, 0, fmt.Errorf("неверный формат: %s", data)
-	}
-
-	steps, err := strconv.Atoi(parts[0])  
-    if err != nil {
-        return 0, 0, fmt.Errorf("не могу прочитать шаги: %v", err)
-    }
-
-    if steps <= 0 {                        
-        return 0, 0, fmt.Errorf("шагов должно быть больше нуля")
-    }
-
-    duration, err := time.ParseDuration(parts[1])  
-    if err != nil {
-        return 0, 0, fmt.Errorf("ошибка времени: %v", err)
-    }
-
-    return steps, duration, nil            
+	// TODO: реализовать функцию
 }
 
-// DayActionInfo возвращает итоговую строку с результатами за день
 func DayActionInfo(data string, weight, height float64) string {
-	 // Вызываем parsePackage
-    steps, duration, err := parsePackage(data)
-    if err != nil {
-        fmt.Println(err)  
-        return ""        
-    }
-
-    // Проверяем что шаги > 0
-    if steps <= 0 {
-        return ""
-    }
-
-    // Считаем дистанцию
-    distanceMeters := float64(steps) * stepLength
-    distanceKm := distanceMeters / mInKm
-
-    // Калории считает готовая функция из другого пакета
-    calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
-    if err != nil {
-        fmt.Println(err)
-        return ""
-    }
-
-    return fmt.Sprintf(
-        "Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
-        steps, distanceKm, calories,
-    )
+	// TODO: реализовать функцию
 }

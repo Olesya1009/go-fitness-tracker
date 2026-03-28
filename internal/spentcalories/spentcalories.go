80cb567352eb3be5e26cdecf61cdcf06f9ca61a7
user <olesyaholod@mail.ru>
2026-03-28 10:39:07 +0300

    fix beg newline

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -81,7 +81,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
             return "", err
         }
         return fmt.Sprintf(
-            "Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+            "Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
             activityType, duration.Hours(), dist, speed, calories,
         ), nil
 
